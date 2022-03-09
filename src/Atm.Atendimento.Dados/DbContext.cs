5f531dd766b173b6d6c4c2babba2b11bafc77128
noh4nsen <noh4nsen@gmail.com>
2022-03-08 22:22:50 -0300

    Implementação de base do microserviço

---
+++ src/Atm.Atendimento.Dados/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Atm.Atendimento.Dados.Extensions;
+using Atm.Atendimento.Dados.Extensions.Facades;
+using Atm.Atendimento.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atm.Atendimento.Dados
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext, IDbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.SetupConstraints();
+            modelBuilder.Setuptables();
+        }
+    }
+}
